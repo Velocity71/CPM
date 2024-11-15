df09e41285f131c2b7e0e86906af133d6bdc66c1
Velocity71 <parker.vannah.71@protonmail.com>
2024-11-15 12:50:02 -0800

    differentiated CPU utilizaton from CPU load

--- Main.c
+++ Main.c
@@ -4,10 +4,17 @@
 int main() {
     float *ptr = get_cpu_load();
 
-    printf("---CPU LOAD---\n");
-    printf("User: %f%%\n", *ptr); ptr++;
-    printf("System: %f%%\n", *ptr); ptr++;
-    printf("Idle: %f%%\n", *ptr); ptr++;
-    printf("Nice: %f%%\n", *ptr);
+    float cpu_user = *ptr; ptr++;
+    float cpu_system = *ptr; ptr++;
+    float cpu_idle = *ptr; ptr++;
+    float cpu_nice = *ptr;
+
+    printf("---CPU Load---\n");
+    printf("Active: %f%%\n", cpu_user + cpu_system);
+    printf("Idle: %f%%\n", cpu_idle);
+    printf("---CPU Utilization---\n");
+    printf("User: %f%%\n", cpu_user);
+    printf("System: %f%%\n", cpu_system);
+    printf("Nice: %f%%\n", cpu_nice);
     return 0;
 }
